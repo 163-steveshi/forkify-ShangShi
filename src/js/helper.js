8af654f2cfd9b2c4b0745675f516acc612ea5555
shang_shi <ShangShi@cmail.carleton.ca>
2023-03-05 13:09:00 -0500

    Update

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,4 +1,3 @@
-
 import { TIMEOUT_SEC } from './config';
 //safety measure for not crash browser--when load take too long
 const timeout = function (s) {
@@ -29,31 +28,4 @@ export const AJAX = async function (url, uploadData = undefined) {
   } catch (err) {
     throw err; //we wants model main part of app handle the err
   }
-};
-
-  // Next 12 lines are cribbed from https://stackoverflow.com/a/23575406.
-  const gcd = function (a, b) {
-    if (b < 0.0000001) {
-      return a;
-    }
-    return gcd(b, Math.floor(a % b));
-  };
-  const len = amount.toString().length - 2;
-  let denominator = Math.pow(10, len);
-  let numerator = amount * denominator;
-  var divisor = gcd(numerator, denominator);
-  numerator /= divisor;
-  denominator /= divisor;
-  let base = 0;
-  // In a scenario like 3/2, convert to 1 1/2
-  // by pulling out the base number and reducing the numerator.
-  if (numerator > denominator) {
-    base = Math.floor(numerator / denominator);
-    numerator -= base * denominator;
-  }
-  amount = Math.floor(numerator) + '/' + Math.floor(denominator);
-  if (base) {
-    amount = base + ' ' + amount;
-  }
-  return amount;
 };
