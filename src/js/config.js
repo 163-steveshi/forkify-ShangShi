5cc0472f2cdf4f27f0cae5c29ac0b6cd630b9dfe
shang_shi <ShangShi@cmail.carleton.ca>
2023-03-05 13:41:39 -0500

    Update

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '8302bef1-82b4-4777-a08c-e3c81694e0ae';
+export const KEY = 'd0a312a0-2a95-41c3-9205-65287349cf9e';
 export const MODAL_CLOSE_SEC = 2.5;
